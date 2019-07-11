ac71feaa0cb4a5e967b1d07555961c754a078efb
koyu <me@koyu.space>
2019-07-11 20:28:42 +0200

    Disabled debugging

--- www/config.js
+++ www/config.js
@@ -1,5 +1,5 @@
-var backend = "http://localhost:9000";
-var debug = true;
+var backend = "https://api.nordcast.app";
+var debug = false;
 
 function onDeviceReady() {
     if (device.platform === "Android" || device.platform === "iOS") {
