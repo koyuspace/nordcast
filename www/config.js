81027c5ffe8d8290e00a029fa827d723e7177d58
koyu <me@koyu.space>
2019-07-17 16:53:44 +0200

    Bump 1.3.5

--- www/config.js
+++ www/config.js
@@ -1,18 +1,11 @@
 var backend = "https://api.nordcast.app";
-var debug = true;
+var debug = false;
 
 function onDeviceReady() {
     if (device.platform === "Android" || device.platform === "iOS") {
         screen.orientation.lock('portrait');
         StatusBar.backgroundColorByHexString("#fff");
     }
-	if (debug) {
-		function handleOpenURL(url) {
-			setTimeout(function() {
-				alert("received url: " + url);
-			}, 0);
-		}
-	}
 }
 
 document.addEventListener("deviceready", onDeviceReady, false);
