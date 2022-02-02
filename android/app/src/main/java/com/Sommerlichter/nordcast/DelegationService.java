4cb169a2bbef177804d1194789e41d7675e76e8b
koyu <me@koyu.space>
2022-02-02 22:11:07 +0100

    Add Android app

---
+++ android/app/src/main/java/com/Sommerlichter/nordcast/DelegationService.java
@@ -0,0 +1,14 @@
+package com.Sommerlichter.nordcast;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
